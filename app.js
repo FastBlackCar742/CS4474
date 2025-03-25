bb7db8f6d729b445e7ea9f6cf788d30ec43ad2e1
Sunny Lin <sunnylin742@gmail.com>
2025-03-24 22:25:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-let map, directionsService, directionsRenderer;
+let map, directionsService, directionsRenderer, originAutocomplete, destinationAutocomplete;
 
 function initMap() {
+    const defaultLocation = { lat: 43.0086, lng: -81.2732 }; // UWO
+
     map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 7,
-        center: { lat: 40.7128, lng: -74.0060 } // Default to New York
+        center: defaultLocation,
+        zoom: 16,
     });
 
     directionsService = new google.maps.DirectionsService();
     directionsRenderer = new google.maps.DirectionsRenderer();
     directionsRenderer.setMap(map);
+
+    // Initialize autocomplete for origin and destination inputs
+    const originInput = document.getElementById("origin");
+    const destinationInput = document.getElementById("destination");
+
+    originAutocomplete = new google.maps.places.Autocomplete(originInput);
+    destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput);
+
+    const westernBounds = new google.maps.LatLngBounds(
+        { lat: 43.004, lng: -81.282 },
+        { lat: 43.012, lng: -81.265 }
+    );
+
+    originAutocomplete.setBounds(westernBounds);
+    destinationAutocomplete.setBounds(westernBounds);
+    originAutocomplete.setOptions({ strictBounds: true });
+    destinationAutocomplete.setOptions({ strictBounds: true });
 }
 
 function getDirections() {
@@ -17,8 +36,8 @@ function getDirections() {
 
     const request = {
         origin: origin,
-        destination: destination,
-        travelMode: 'DRIVING'
+            destination: destination,
+        travelMode: google.maps.TravelMode.WALKING // Mode of transportation
     };
 
     directionsService.route(request, function (result, status) {
